5205cf766e04fea43bd8fff883f28c9125943e1b
yakubalkis <yakupalkiis@gmail.com>
2022-05-05 19:31:59 +0300

    Modal Window project is done.

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.close-modal');
+const btnsOpenModal=document.querySelectorAll('.show-modal');
+
+function closeModal(){ //functions to be able to open or close the modal
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+function openModal(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for(let i=0;i<btnsOpenModal.length;i++){ //events on modal button
+btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+}
+
+document.addEventListener('keydown', function(e){ //event of keydown "Espace"
+    if(e.key==="Escape" && !modal.classList.contains('hidden')){
+            closeModal();
+    }
+});
+
+
